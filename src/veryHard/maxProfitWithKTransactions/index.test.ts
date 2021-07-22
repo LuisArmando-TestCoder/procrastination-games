f1414409bd787303ca05d800819033bdabcb828d
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2021-07-22 00:59:04 -0600

    add complete maxProfitWithKTransactions

--- src/veryHard/maxProfitWithKTransactions/index.test.ts
+++ src/veryHard/maxProfitWithKTransactions/index.test.ts
@@ -38,6 +38,22 @@ import maxProfitWithKTransactions from '.'
 
 test('Get the best max profit with k transactions', () => {
     expect(
-        maxProfitWithKTransactions([5, 11, 3, 50, 60, 90], 2)
-    ).toBe(93)
+        maxProfitWithKTransactions([5, 11, 3, 90, 8, 50, 1, 30], 2)
+    ).toBe(129)
+
+    expect(
+        maxProfitWithKTransactions([5, 11, 3, 90, 8, 50, 1, 30], 1)
+    ).toBe(87)
+
+    expect(
+        maxProfitWithKTransactions([5, 11, 3, 90, 8, 50, 1, 30], 3)
+    ).toBe(158)
+
+    expect(
+        maxProfitWithKTransactions([5, 11, 3, 90, 8, 50, 1, 30], 4)
+    ).toBe(164)
+
+    expect(
+        maxProfitWithKTransactions([5, 11, 3, 80], 2)
+    ).toBe(83)
 })
--- src/veryHard/maxProfitWithKTransactions/index.ts
+++ src/veryHard/maxProfitWithKTransactions/index.ts
@@ -1,13 +1,13 @@
 function getAllInmediateTransactions(stockPrices: number[]): number[][] {
     const prices = [...stockPrices]
-    const {length} = prices
+    const { length } = prices
     const buys: number[] = []
     const sells: number[] = []
 
     buys.push(...prices.splice(0, 1))
 
     for (let i = 0; i < length; i++) {
-        const haveSellsBePushed = buys.length === sells.length // can be deleted, and replaced continue 
+        const haveSellsBePushed = buys.length === sells.length
         const haveBuysBePushed = buys.length > sells.length
         const price = prices[i]
         const lastBuy = buys[buys.length - 1]
@@ -31,20 +31,44 @@ function getAllInmediateTransactions(stockPrices: number[]): number[][] {
     return [buys, sells]
 }
 
-function getTransactionsProfit(buys: number[], sells: number[]) {
-    const getSummation = (
-        group: number[]
-    ) => group.reduce((a: number, b: number) => a + b)
-    const buysSummation = getSummation(buys)
-    const sellsSummation = getSummation(sells)
+function getAllPosibleProfits({
+    buys,
+    sells,
+    transactionsAmount,
+    profits = [],
+    previousProfit = 0,
+}: {
+    buys: number[]
+    sells: number[]
+    transactionsAmount: number
+    profits?: number[]
+    previousProfit?: number
+}) {
+    sells.forEach((sell, sellIndex) => {
+        buys.slice(0, sellIndex + 1).forEach(buy => {
+            const newProfit = sell - buy + previousProfit
 
-    return sellsSummation - buysSummation
+            profits.push(newProfit)
+
+            if (transactionsAmount > 1) {
+                getAllPosibleProfits({
+                    buys: buys.slice(sellIndex + 1),
+                    sells: sells.slice(sellIndex + 1),
+                    previousProfit: newProfit,
+                    profits,
+                    transactionsAmount: transactionsAmount - 1,
+                })
+            }
+        })
+    })
+
+    return profits
 }
 
 export default (stockPrices: number[], transactionsAmount: number) => {
     const [buys, sells] = getAllInmediateTransactions(stockPrices)
-    const transactionsSummation = getTransactionsProfit(buys, sells)
-    // still pending integrate k into max profit for transactions
+    const allPosibleProfits = getAllPosibleProfits({buys, sells, transactionsAmount})
+    const [maximumProfit] = allPosibleProfits.sort((a, b) => b - a)
 
-    return transactionsSummation
+    return maximumProfit
 }
