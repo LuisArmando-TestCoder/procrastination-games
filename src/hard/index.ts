39620e3e2758489f13401abfcf8f0f4f03c0cea8
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2021-07-26 12:19:08 -0600

    add getSquareSums

--- src/hard/index.ts
+++ src/hard/index.ts
@@ -1,7 +1,5 @@
-import * as getSquareSums from './getSquareSums'
 import * as shiftLinkedList from './shiftLinkedList'
 
 export {
-   getSquareSums,
    shiftLinkedList,
 }
--- src/veryHard/index.ts
+++ src/veryHard/index.ts
@@ -1,5 +1,9 @@
+import * as getSquareSums from './getSquareSums'
 import * as maxProfitWithKTransactions from './maxProfitWithKTransactions'
+import * as nQueensProblem from './nQueensProblem'
 
 export {
+   getSquareSums,
    maxProfitWithKTransactions,
+   nQueensProblem,
 }
